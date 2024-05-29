2d141fb63aa4a70d0c317070a60b259e356e232a
Hassan-Imtiaz3 <82123163+hassanimtiaz194@users.noreply.github.com>
2024-05-29 10:10:02 +0500

    Initial commit

---
+++ vue-todos/vue-todos/src/components/TodoCreator.vue
@@ -0,0 +1,56 @@
+<script setup>
+import { ref, reactive, defineEmits } from 'vue';
+
+const emit = defineEmits(["create-todo"]);
+
+// ref is use to make our data reactive
+const todo = ref("");
+// another way to make data reactive
+const todoState = reactive({
+    todo: "testing reactive"
+});
+
+const createTodo = () => {
+    console.log('dsdsd');
+    emit('create-todo', todo.value);
+};
+</script>
+
+<template>
+    <div>
+        <div class="input-wrap">
+            <input type="text" v-model="todo" /> <!-- v-model="todoState.todo" -->
+            <button @click="createTodo">Create</button>
+        </div>
+    </div>
+    <!--     <p>{{ todo }}</p> -->
+    <!--     <p>{{ todoState.todo }}</p> -->
+</template>
+
+<style lang="scss" scoped>
+.input-wrap {
+    display: flex;
+    transition: 250ms ease;
+    border: 2px solid #41b080;
+
+    &:focus-within {
+        box-shadow: 0 -4px 6px -1px rgb(0 0 0 / 0.1),
+            0 -2px 4px -2px rgb(0 0 0 / 0.1);
+    }
+
+    input {
+        width: 100%;
+        padding: 8px 6px;
+        border: none;
+
+        &:focus {
+            outline: none;
+        }
+    }
+
+    button {
+        padding: 8px 16px;
+        border: none;
+    }
+}
+</style>
---
+++ vue-todos/vue-todos/src/views/TodosView.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { ref } from 'vue';
+import { uid } from 'uid';
+import TodoCreator from '../components/TodoCreator.vue';
+const todoList = ref([]);
+
+const createTodo = (todo) => {
+  console.log(todo);
+  console.log(uid());
+  todoList.value.push({
+    id: uid(),
+    todo,
+    isCompleted: null,
+    isEditing: null,
+  });
+}
+console.log(todoList);
+</script>
+
+<template>
+  <main>
+    <h1>Create Todo</h1>
+    <TodoCreator @create-todo="createTodo" />
+  </main>
+</template>
+
+<style lang="scss" scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  max-width: 500px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 40px 16px;
+
+  h1 {
+    margin-bottom: 16px;
+    text-align: center;
+  }
+}
+</style>
